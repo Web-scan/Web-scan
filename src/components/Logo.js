581cb63a321f654419fa6ac7e1d53b4859a26c6c
Sohyeon <needtlc13@gmail.com>
2022-11-10 23:04:03 +0900

    style: design scan and edit file pages

---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+import { useNavigate } from "react-router-dom";
+
+import logo from "../assets/logo.svg";
+
+export default function Logo() {
+  const navigate = useNavigate();
+
+  return <img src={logo} alt="logo" onClick={() => navigate("/")} />;
+}
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,27 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
 import PropTypes from "prop-types";
 
+import { GREY_50, GREY_100 } from "../../constants/color";
+
 export default function Header({ children }) {
-  return <div>{children}</div>;
+  return (
+    <div
+      css={css({
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "20px 30px",
+        width: "100%",
+        height: "86px",
+        backgroundColor: GREY_50,
+        boxShadow: `0 0 6px ${GREY_100}`,
+      })}
+    >
+      {children}
+    </div>
+  );
 }
 
 Header.propTypes = {
