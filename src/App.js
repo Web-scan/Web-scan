e03e84725c08c0bfa0d4a2e7cddc481d28b77cdf
Ye sohyeon <93248242+yesohyeon@users.noreply.github.com>
2022-11-11 09:23:16 +0900

    refactor: set global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { HashRouter } from "react-router-dom";
 
 import Layout from "../src/components/layout";
+import GlobalStyle from "./styles/global";
 import Router from "./Router";
 
 function App() {
   return (
     <HashRouter>
+      <GlobalStyle />
       <Layout>
         <Router />
       </Layout>
