b82e494b048e3780d497bd4088b73fbb743ead0b
Sohyeon <needtlc13@gmail.com>
2022-11-18 21:31:16 +0900

    refactor: drag code state down to code  each area components

---
+++ src/components/editFile/ScannedComponentCodeArea.js
@@ -0,0 +1,37 @@
+/** @jsxImportSource @emotion/react */
+
+import { useCallback } from "react";
+import { useRecoilState } from "recoil";
+
+import Editor from "../shared/Editor";
+
+import { CODE_AREA } from "../../constants/ui";
+import { GREY_150 } from "../../constants/color";
+import scannedElementComponentCodeState from "../../recoil/scannedElementComponentCode";
+
+export default function ScannedComponentCodeArea() {
+  const [scannedElementComponentCode, setScannedElementComponentCode] =
+    useRecoilState(scannedElementComponentCodeState);
+
+  const handleCodeChange = useCallback((code) => {
+    setScannedElementComponentCode(code);
+  }, []);
+
+  return (
+    <>
+      <div css={{ ...textStyle, height: "40px" }}>
+        {CODE_AREA.SCANNED_ELEMENT_COMPONENT_CODE}
+      </div>
+      <Editor
+        code={scannedElementComponentCode}
+        handleChange={handleCodeChange}
+        width={(window.innerWidth * 0.5 - 36).toString() + "px"}
+      />
+    </>
+  );
+}
+
+const textStyle = {
+  fontSize: "16px",
+  color: GREY_150,
+};
--- src/pages/EditFile.js
+++ src/pages/EditFile.js
@@ -1,58 +1,15 @@
 /** @jsxImportSource @emotion/react */
 
-import { useState, useCallback } from "react";
-
-import fs from "fs";
-import { useRecoilState } from "recoil";
-
 import Header from "../components/layout/Header";
 import ContentBox from "../components/layout/ContentBox";
 
-import ScannedComponentCode from "../components/editFile/ScannedComponentCode";
-import LoadedFileCode from "../components/editFile/LoadedFileCode";
+import ScannedComponentCodeArea from "../components/editFile/ScannedComponentCodeArea";
+import LoadedFileCodeArea from "../components/editFile/LoadedFileCodeArea";
 import Logo from "../components/shared/Logo";
 
-import scannedElementComponentCodeState from "../recoil/scannedElementComponentCode";
-import loadedFileCodeState from "../recoil/loadedFileCode";
-
 import { GREY_50 } from "../constants/color";
-import { SAVE_CODE } from "../constants/ui";
 
 export default function EditFile() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [filePath, setFilePath] = useState("");
-  const [saveResult, setSaveResult] = useState("");
-  const [scannedElementComponentCode, setScannedElementComponentCode] =
-    useRecoilState(scannedElementComponentCodeState);
-  const [loadedFileCode, setLoadedFileCode] =
-    useRecoilState(loadedFileCodeState);
-
-  const handleScannedElementCodeChange = useCallback((code) => {
-    setScannedElementComponentCode(code);
-  }, []);
-
-  const handleFileCodeChange = useCallback((code) => {
-    setLoadedFileCode(code);
-  }, []);
-
-  const handleSaveClick = async () => {
-    try {
-      await fs.writeFile(filePath, loadedFileCode, (err) => {
-        if (err) {
-          setIsModalOpen(true);
-          setSaveResult(SAVE_CODE.FAIL);
-          return;
-        }
-
-        setIsModalOpen(true);
-        setSaveResult(SAVE_CODE.SUCCESS);
-      });
-    } catch (e) {
-      setIsModalOpen(true);
-      setSaveResult(SAVE_CODE.SUCCESS);
-    }
-  };
-
   return (
     <>
       <Header>
@@ -60,10 +17,7 @@ export default function EditFile() {
       </Header>
       <ContentBox>
         <div css={wrapper}>
-          <ScannedComponentCode
-            code={scannedElementComponentCode}
-            handleChange={handleScannedElementCodeChange}
-          />
+          <ScannedComponentCodeArea />
         </div>
         <div
           css={{
@@ -71,15 +25,7 @@ export default function EditFile() {
             borderLeft: `1px solid ${GREY_50}`,
           }}
         >
-          <LoadedFileCode
-            code={loadedFileCode}
-            handleChange={handleFileCodeChange}
-            handleClick={handleSaveClick}
-            isModalOpen={isModalOpen}
-            handleModalClose={() => setIsModalOpen(false)}
-            saveResult={saveResult}
-            handleFilePath={(path) => setFilePath(path)}
-          />
+          <LoadedFileCodeArea />
         </div>
       </ContentBox>
     </>
