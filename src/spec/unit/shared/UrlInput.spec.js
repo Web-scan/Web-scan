9dea3a7159ec79c4342041ee58a04818b9b5f7e6
Sohyeon <needtlc13@gmail.com>
2022-11-21 13:44:19 +0900

    test: write unit test for url input component

---
+++ src/spec/unit/shared/UrlInput.spec.js
@@ -0,0 +1,30 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import UrlInput from "../../../components/shared/UrlInput";
+
+describe("<UrlInput />", () => {
+  it("Display correct value and placeholder. Also both handleChange and handleSubmit should work", () => {
+    const value = "test text";
+    const handleChange = jest.fn();
+    const handleSubmit = jest.fn();
+    const placeholder = "test placeholder";
+
+    render(
+      <UrlInput
+        value={value}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        placeholder={placeholder}
+      />,
+    );
+
+    const inputBar = screen.getByRole("textbox");
+    expect(inputBar).toHaveAttribute("placeholder", placeholder);
+    expect(inputBar.value).toBe(value);
+
+    fireEvent.change(inputBar, { target: { value: "text change" } });
+    expect(handleChange).toHaveBeenCalled();
+
+    fireEvent.submit(inputBar);
+    expect(handleSubmit).toHaveBeenCalled();
+  });
+});
